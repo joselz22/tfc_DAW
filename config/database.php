bd75dce4be087cb2af28fd6f268e968c3f69435a
joselz22 <jalv9722@gmail.com>
2026-03-01 13:09:54 +0100

    Estructura inicial del proyecto y conexión a la base de datos

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $dbname = "centro_comercial";
+
+    try {
+        $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }catch (PDOException $e){
+        die("Error de conexión: ". $e->getMessage());
+    }
